bf45c4d120dc243c210c3262e8ff884ecef1e6f3
jpb06 <jp.bois.06@outlook.fr>
2023-01-31 20:23:21 +0100

    chore: testing env var conditional usage

--- src/logic/fetching/fetch-graphql-schema.test.ts
+++ src/logic/fetching/fetch-graphql-schema.test.ts
@@ -7,12 +7,45 @@ jest.mock('axios');
 
 describe('fetchGraphqlSchema function', () => {
   it('should return schema types', async () => {
+    const url = 'https://yolo.cool';
     jest.mocked(axios.post).mockResolvedValueOnce({
       data: schema,
     });
 
-    const result = await fetchGraphqlSchema('https://yolo.cool');
+    const result = await fetchGraphqlSchema(url);
 
     expect(result).toHaveLength(39);
+    expect(axios.post).toHaveBeenCalledWith(url, expect.any(Object));
+  });
+
+  it('should use post fix env var name when it does not exist', async () => {
+    const url = 'API_URL';
+
+    jest.mocked(axios.post).mockResolvedValueOnce({
+      data: schema,
+    });
+
+    const result = await fetchGraphqlSchema(url);
+
+    expect(result).toHaveLength(39);
+    expect(axios.post).toHaveBeenCalledWith(
+      'API_URL-not-set',
+      expect.any(Object),
+    );
+  });
+
+  it('should use environment variables value', async () => {
+    const url = 'https://yolo.cool';
+    process.env.API_URL = url;
+    const input = 'API_URL';
+
+    jest.mocked(axios.post).mockResolvedValueOnce({
+      data: schema,
+    });
+
+    const result = await fetchGraphqlSchema(input);
+
+    expect(result).toHaveLength(39);
+    expect(axios.post).toHaveBeenCalledWith(url, expect.any(Object));
   });
 });
