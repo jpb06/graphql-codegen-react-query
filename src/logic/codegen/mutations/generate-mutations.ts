c4667b9ec7e138dcd6061762c02c554cf0a294dc
JP Bois <jp.bois.06@outlook.fr>
2023-03-05 16:37:27 +0100

    chore: fixing mutation args type when there is no args (#19)
    * chore: fixing mutation args type when there is no args
    
    * chore: extracting ternaries

--- src/logic/codegen/mutations/generate-mutations.ts
+++ src/logic/codegen/mutations/generate-mutations.ts
@@ -3,6 +3,7 @@ import { writeFile } from 'fs-extra';
 import { mutationHookContent } from './content/mutation-hook-content';
 import { generateDocument } from './document/generate-document';
 import { getMutationResultTypes } from './result-type/get-mutation-result-types';
+import { getTypesImports } from './types-imports/get-types-imports';
 import { FetcherConfig } from '../../../cli/generate-from-url/args-validation/options-validation';
 import { GqlField } from '../../../types/introspection-query-response.type';
 import { capitalize } from '../../util/capitalize';
@@ -21,27 +22,27 @@ export const generateMutations = async (
   for (const { type, name, args } of fields) {
     const mutationType = capitalize(name);
     const mutationArgsType =
-      args?.length && args.length > 0 ? `${mutationType}MutationArgs` : 'never';
+      args?.length && args.length > 0
+        ? `${mutationType}MutationArgs`
+        : 'unknown';
 
     const { documentResultFields, resultType } = getMutationResultTypes(
       type,
       types,
     );
     const document = generateDocument(name, args, documentResultFields);
+    const typesImports = getTypesImports(
+      mutationArgsType,
+      rootObjectsName,
+      resultType,
+    );
 
     const data = mutationHookContent
       .replace(
         '#fetcher-hook-import#',
         `import { ${fetcher.functionName} } from '${fetcher.path}';`,
       )
-      .replace(
-        '#mutation-types-import#',
-        `import {${
-          mutationArgsType !== 'never' ? ` ${mutationArgsType} ` : ''
-        } ${
-          rootObjectsName.includes(resultType) ? `, ${resultType}` : ''
-        } } from '../types/api-types';`,
-      )
+      .replace('#mutation-types-import#', typesImports)
       .replace('#name#', name)
       .replace(/#mutation-result-type#/g, resultType)
       .replace(/#mutation-args-type#/g, mutationArgsType)
